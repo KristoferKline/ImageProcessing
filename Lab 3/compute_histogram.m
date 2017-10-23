541042604604a9a7c8974d866015a819ac7e7fd8
kris <kristkline@gmail.com>
2017-10-22 18:54:28 -0700

    Added documentation

--- Lab 3/compute_histogram.m
+++ Lab 3/compute_histogram.m
@@ -10,10 +10,14 @@ function histogram = compute_histogram(image)
 % image                 An array of UINT8 representing intensity levels of
 %                       a grayscale image.
 
+% Initialize dimensions of the image.
 width = size(image,2);
 height = size(image,1);
 
+% Optimization: Initialize the histogram for space allocation.
 histogram = zeros(256,1,'double');
+
+% Iterate through the image, inrementing the corresponding count.
 for w = 1:width
     for h = 1:height
         intensity = image(w * h) + 1;
@@ -21,6 +25,7 @@ for w = 1:width
     end
 end
 
+% Divide the entire matrix by 256 to create a percentage, PMF.
 for i = 1:256
     histogram(i) = histogram(i) / (width * height);
 end
--- Lab 3/histogram_transform.m
+++ Lab 3/histogram_transform.m
@@ -10,15 +10,25 @@ function transformation = histogram_transform(histogram)
 % histogram                     A matrix of UINT8 representing the
 %                               histogram of an image.
 
+% Range of the histogram, l representing the number of levels.
 l = size(histogram,1);
 
+% Optimization: Initialize cumulative distribution and set the first value.
 cmf = zeros(l,1,'double');
 cmf(1) = histogram(1);
+
+% Iterate through the PMF, adding the previously stored result.
 for i = 2:l
     cmf(i) = cmf(i-1) + (l - 1) * histogram(i);
 end
 
+% Optimization: Initialize result transformation with range of levels.
 transformation = zeros(l,1,'uint8');
+
+% Iterate through the CMF, rounding the values to create the
+% transformation.
 for i = 1:l
     transformation(i) = round(cmf(i));
+end
+
 end
